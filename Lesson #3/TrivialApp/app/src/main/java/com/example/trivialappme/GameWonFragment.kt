296f3223b07c9dca6f555957f944c3d290b2644b
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-08-07 22:02:46 +0200

    Starting Lesson #3 Project (Trivia)

---
+++ Lesson #3/TrivialApp/app/src/main/java/com/example/trivialappme/GameWonFragment.kt
@@ -0,0 +1,23 @@
+
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+//import com.example.android.navigation.databinding.FragmentGameWonBinding
+import com.example.trivialappme.R
+import com.example.trivialappme.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_game_won, container, false)
+        return binding.root
+    }
+}
