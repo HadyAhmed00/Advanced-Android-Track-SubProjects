1035d1ca8d3e22b38588ded1bbf922071c05e56e
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-09-06 16:19:19 +0200

    Finishing the Notification and using firebase Messages

---
+++ Part#3/Lesson#1/EggTimer/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,66 @@
+package com.example.android.eggtimernotifications
+
+import android.app.NotificationManager
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        /* // Check if message contains a data payload.
+         if (remoteMessage.data.isNotEmpty()) {
+             Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+             if (*//* Check if data needs to be processed by long running job *//* true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+                handleNow()
+            }
+        }
+*/
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+
+            sendNotification(it.body!!)
+
+
+        }
+
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    private fun sendNotification(body: String) {
+//        TODO("Not yet implemented")
+        val notificationManager = ContextCompat.getSystemService(applicationContext,NotificationManager::class.java)as NotificationManager
+        notificationManager.sendNotification(body,applicationContext)
+    }
+
+
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String) {
+//        TODO("Not yet implemented")
+    }
+}
--- Part#3/Lesson#1/EggTimer/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ Part#3/Lesson#1/EggTimer/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -51,14 +51,50 @@ class EggTimerFragment : Fragment() {
         binding.lifecycleOwner = this.viewLifecycleOwner
 
         // TODO: Step 1.7 call create channel
-
+        createChannel(
+            getString(R.string.egg_notification_channel_id),
+            getString(R.string.egg_notification_channel_name)
+        )
+        subscribeToTopic()
         return binding.root
     }
 
     private fun createChannel(channelId: String, channelName: String) {
         // TODO: Step 1.6 START create a channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                // TODO: Step 2.4 change importance
+                NotificationManager.IMPORTANCE_HIGH
+            )// TODO: Step 2.6 disable badges for this channel
+                .apply {
+                    setShowBadge(false)
+                }
 
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.breakfast_notification_channel_description)
+
+            val notificationManager = requireActivity().getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
         // TODO: Step 1.6 END create a channel
+    }
+
+
+    fun subscribeToTopic(){
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC).addOnCompleteListener{
+            var massage = getString(R.string.message_subscribed)
+            if(!it.isSuccessful){
+                massage= getString(R.string.message_subscribe_failed)
+            }
+            Toast.makeText(this.context, massage, Toast.LENGTH_SHORT).show()
+        }
 
     }
 
