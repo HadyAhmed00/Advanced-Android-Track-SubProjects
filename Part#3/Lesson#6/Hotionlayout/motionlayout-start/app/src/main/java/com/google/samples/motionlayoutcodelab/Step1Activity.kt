3b347d3e734ed2bcc846e31f6956ebcd1458a07f
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-09-09 11:00:16 +0200

    Finishing the animation Lesson

--- Part#3/Lesson#6/Hotionlayout/motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step1Activity.kt
+++ Part#3/Lesson#6/Hotionlayout/motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/Step1Activity.kt
@@ -26,7 +26,7 @@ class Step1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_step1_completed)
+        setContentView(R.layout.activity_step1)
     }
 
 }
